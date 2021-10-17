26068185d4b4e54069f49d71a42eedac903acb85
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2021-10-17 08:08:09 -0600

    update

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+console.log('hye')
+
+
+class LinkedList {
+  val: string
+  next: LinkedList | undefined
+  constructor(val: string) {
+    this.val = val
+  }
+}
+
+
+let a = new LinkedList('a');
+
+
+
+let b = new LinkedList('b');
+
+
+console.log(a)
+
+
+
+console.log('abc')
